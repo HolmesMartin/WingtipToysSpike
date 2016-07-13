c2cbfef05692bf70b7fd3777b7e18343ab4e8026
Martin Holmes <holmesmrtn@aol.com>
2016-07-13 13:13:01 -0400

    Add project files.

---
+++ WingtipToysSpike/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WingtipToysSpike.Startup))]
+namespace WingtipToysSpike
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
