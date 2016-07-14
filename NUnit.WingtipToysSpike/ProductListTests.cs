ec68bde32fa2049110e44804a3e623ae9a5c221e
Martin Holmes <holmesmrtn@aol.com>
2016-07-14 15:48:38 -0400

    I dunno.  I guess some changes were made

---
+++ NUnit.WingtipToysSpike/ProductListTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WingtipToysSpike;
+
+namespace NUnit.WingtipToysSpike
+{
+    [TestFixture]
+    public class ProductListTests
+    {
+
+
+        [Test]
+        public void TestPageLoad()
+        {
+            Assert.AreEqual(0,0);
+        }
+    }
+}
